abda7a850a9aaef79a350d097f4c28ce3f953da4
alealonso17 <2alejandroalonso70@gmail.com>
2025-12-26 16:14:41 +0100

    Initial Commit

---
+++ HarryPotterDemo/HarryPotterDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarryPotterDemoApp.swift
+//  HarryPotterDemo
+//
+//  Created by Alejandro Alonso on 26/12/25.
+//
+
+import SwiftUI
+
+@main
+struct HarryPotterDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HarryPotterDemoTests/HarryPotterDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  HarryPotterDemoTests.swift
+//  HarryPotterDemoTests
+//
+//  Created by Alejandro Alonso on 26/12/25.
+//
+
+import Testing
+@testable import HarryPotterDemo
+
+struct HarryPotterDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
